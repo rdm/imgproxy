a0ebba49bc096f196beea4d22f992b1bbc59f292
Raul Miller <rauldmiller@gmail.com>
2019-05-20 15:53:59 -0400

    and... deeper...

--- crypt.go
+++ crypt.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"fmt"
 )
@@ -20,10 +21,10 @@ func validatePath(signature, path string) error {
 	if err != nil {
 		return errInvalidSignatureEncoding
 	}
-	fmt.Printf("messageMAC: %s\n", messageMAC);
+	fmt.Printf("messageMAC %d: %s\n", len(messageMAC), hex.EncodeToString(messageMAC));
 
 	for i := 0; i < len(conf.Keys); i++ {
-		fmt.Printf("i: %d, conf.Key[i]: %s\n", i, conf.Keys[i]);
+		fmt.Printf("i: %d, conf.Key[i]: %s\n", i, hex.EncodeToString(conf.Keys[i]));
 		if hmac.Equal(messageMAC, signatureFor(path, i)) {
 			return nil
 		}
@@ -34,9 +35,15 @@ func validatePath(signature, path string) error {
 
 func signatureFor(str string, pairInd int) []byte {
 	mac := hmac.New(sha256.New, conf.Keys[pairInd])
+	fmt.Printf("pairInd: %d\n", pairInd)
+	fmt.Printf("key: %d: %s\n", len(conf.Keys[pairInd]), hex.EncodeToString(conf.Keys[pairInd]))
 	mac.Write(conf.Salts[pairInd])
+	fmt.Printf("salt: %d: %s\n", len(conf.Salts[pairInd]), hex.EncodeToString(conf.Salts[pairInd]))
+	fmt.Printf("str: %d: %s\n", len(str), str)
 	mac.Write([]byte(str))
 	expectedMAC := mac.Sum(nil)
+	fmt.Printf("expectedMAC: %d -- %s\n", len(expectedMAC), expectedMAC)
+	fmt.Printf("conf.SignatureSize: %d\n", conf.SignatureSize);
 	if conf.SignatureSize < 32 {
 		return expectedMAC[:conf.SignatureSize]
 	}
